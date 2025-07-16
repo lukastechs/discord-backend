75a89e3b28acc4920007bcc35fae17a648625eb5
Egbujor Victor Chinedu <33014882+lukastechs@users.noreply.github.com>
2025-07-16 15:10:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const discordBackend = require('.discord-backend');
+const discordBackend = require('./api/discord-backend');
 
 const app = express();
 app.use(express.json());
